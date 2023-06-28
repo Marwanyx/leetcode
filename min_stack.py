cb6ad0e497e1fa81be3c9e2379d5ef4e2fea2a1b
DESKTOP-S190SD7 <speedmero@hotmail.com>
2023-06-28 00:55:59 -0400

    comments

--- min_stack.py
+++ min_stack.py
@@ -16,12 +16,3 @@ class MinStack:
         
     def getMin(self) -> int:
         return min(self.items)
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
