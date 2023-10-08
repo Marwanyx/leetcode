cd51d673df16489cbc4cc9f0938ca14b3f096fc7
Marwan Yousef <marwan.yousef@mail.utoronto.ca>
2023-10-08 14:33:26 -0400

    m

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -2,38 +2,26 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        map = {}
-        result = []
-        minRes = []
+        if not strs:
+            return []
+        res = []
+        keys = []
+        flag = False
+        for i in range(len(strs)):
+            temp = "".join(sorted(strs[i]))
+            for j in range(len(keys)):
+                if temp in keys[j]:
+                    res[j].append(strs[i])
+                    flag = True
+            if not flag:
+                res.append([strs[i]])
+                keys.append([temp])
+            flag = False
 
-        for word in strs:
-            char = []
-            for letter in word:
-                char.append(letter)
-            char.sort()
-            map[word] = char
-
-        # "eat": ["a", "e", "t"]
-        # "tea": ["a", "e", "t"]
-        nextMap = dict(map)
-        while map != {}:
-            minRes = []
-            nextMap = {}
-            temp = list(map.values())
-            val = temp[0]
-            for words in map:
-                if map[words] == val:
-                    minRes.append(words)
-                else:
-                    nextMap[words] = map[words]
-            map = dict(nextMap)
-                    
-            result.append(minRes)
-
-        return result
-    
+        return res
 
 s = Solution()
+print(s.groupAnagrams(["a"]))
 print(s.groupAnagrams([""]))
 print(s.groupAnagrams(["",""]))
 print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
