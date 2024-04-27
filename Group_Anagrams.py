ee605e2963d5064800233feb355898e621c00766
Marwan <marwan.yousef@mail.utoronto.ca>
2024-04-26 20:56:16 -0400

    anotha one

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -2,26 +2,12 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if not strs:
-            return []
-        res = []
-        keys = []
-        flag = False
-        for i in range(len(strs)):
-            temp = "".join(sorted(strs[i]))
-            for j in range(len(keys)):
-                if temp in keys[j]:
-                    res[j].append(strs[i])
-                    flag = True
-            if not flag:
-                res.append([strs[i]])
-                keys.append([temp])
-            flag = False
-
-        return res
-
-s = Solution()
-print(s.groupAnagrams(["a"]))
-print(s.groupAnagrams([""]))
-print(s.groupAnagrams(["",""]))
-print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+        dct = {}
+        for stri in strs:
+            newStr = "".join(sorted(stri))
+            if newStr in dct:
+                dct[newStr].append(stri)
+            else:
+                dct[newStr] = [stri]
+                
+        return dct.values()
