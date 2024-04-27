65934e50b4eaa96f72d3230cd7e7093dfe50a3ef
Marwan <marwan.yousef@mail.utoronto.ca>
2024-04-26 21:06:25 -0400

    anotha one

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -1,18 +1,10 @@
-from typing import List
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
+        hashmap = defaultdict(lambda:1)
         for num in nums:
-            if num not in hashmap:
-                hashmap[num] = 1
-            else:
-                hashmap[num] += 1
+            hashmap[num] += 1
 
-        # sort the hashmap by value, greater to smaller
-        sorted_hashmap = dict(sorted(hashmap.items(), key=lambda x: x[1], reverse=True))
-        return list(sorted_hashmap.keys())[:k]
-    
-s = Solution()
-print(s.topKFrequent([1,1,1,2,2,3], 2)) # [1,2]
-        
+        newdict = dict(sorted(hashmap.items(), key=lambda item: item[1], reverse=True))
+        newdict = list(newdict.keys())
+        return newdict[:k]
+
