872a5bc615d9deaa58ebcf0e3934825fb375b94c
Marwan <marwan.yousef@mail.utoronto.ca>
2024-04-28 00:29:10 -0400

    125

---
+++ is_palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result = ""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                result += s[i]
+        s = result.lower()
+        
+        return s == s[::-1]
